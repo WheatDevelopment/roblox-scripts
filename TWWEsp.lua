d575cf24a0e29f6bf24f3816598ef64fd513217d
WheatDevelopment <richard.falkenau@gmx.de>
2025-03-15 11:32:28 +0100

    Create TWWEsp.lua

---
+++ TWWEsp.lua
@@ -0,0 +1,68 @@
+local RunService = game:GetService("RunService")
+local Camera = workspace.CurrentCamera
+
+_G.ESPVisible = true
+_G.TextColor = Color3.fromRGB(255, 80, 10)
+_G.TextSize = 14
+_G.Center = true
+_G.Outline = true
+_G.OutlineColor = Color3.fromRGB(0, 0, 0)
+_G.TextTransparency = 0.7
+_G.TextFont = Drawing.Fonts.UI
+_G.DisableKey = Enum.KeyCode.Z
+
+local UserInputService = game:GetService("UserInputService")
+local Typing = false
+local ESPObjects = {}
+
+local function CreateESPForAnimals()
+    for _, animal in pairs(workspace.WORKSPACE_Entities.Animals:GetChildren()) do
+        if animal:IsA("Model") and animal:FindFirstChild("HumanoidRootPart") then
+            local ESP = Drawing.new("Text")
+            ESPObjects[animal] = ESP
+
+            RunService.RenderStepped:Connect(function()
+                if _G.ESPVisible and animal and animal:FindFirstChild("HumanoidRootPart") then
+                    local Vector, OnScreen = Camera:WorldToViewportPoint(animal.HumanoidRootPart.Position)
+
+                    ESP.Size = _G.TextSize
+                    ESP.Center = _G.Center
+                    ESP.Outline = _G.Outline
+                    ESP.OutlineColor = _G.OutlineColor
+                    ESP.Color = _G.TextColor
+                    ESP.Transparency = _G.TextTransparency
+                    ESP.Font = _G.TextFont
+
+                    if OnScreen then
+                        ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                        ESP.Text = animal.Name
+                        ESP.Visible = true
+                    else
+                        ESP.Visible = false
+                    end
+                else
+                    ESP.Visible = false
+                end
+            end)
+        end
+    end
+end
+
+UserInputService.TextBoxFocused:Connect(function()
+    Typing = true
+end)
+
+UserInputService.TextBoxFocusReleased:Connect(function()
+    Typing = false
+end)
+
+UserInputService.InputBegan:Connect(function(Input)
+    if Input.KeyCode == _G.DisableKey and not Typing then
+        _G.ESPVisible = not _G.ESPVisible
+        for _, ESP in pairs(ESPObjects) do
+            ESP.Visible = _G.ESPVisible
+        end
+    end
+end)
+
+CreateESPForAnimals()
